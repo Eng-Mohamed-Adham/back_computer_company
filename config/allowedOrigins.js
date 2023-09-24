a7a1444c5b9e91c9a65a83ba7ba97b94e3675119
Eng-Mohamed-Adham <pythonh37@gmail.com>
2023-09-24 11:48:04 +0300

    new-update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,6 @@
 
 const allowedOrigins = [
-    'http://localhost:3000',
-    'https://www.dandrepairshop.com',
-    'https://dandrepairshop.com',
+    'https://goldentech.onrender.com',
 ]
 
 module.exports = allowedOrigins
